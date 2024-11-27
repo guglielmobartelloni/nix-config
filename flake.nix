431d75787df27d9f17c45df06ca7660ecfd294d8
samoorai <bartelloni.guglielmo@gmail.com>
2024-11-27 21:18:20 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "Guglielmo's nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nix-homebrew, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      nixpkgs.config.allowUnfree = true;
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.neovim
+          pkgs.kitty
+          pkgs.raycast
+        ];
+
+      homebrew = {
+          enable = true;
+          casks = [
+            "firefox"
+            "iina"
+            "the-unarchiver"
+          ];
+          onActivation.cleanup = "zap";
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."samurai" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+            {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "samurai";
+            autoMigrate = true;
+          };
+          }
+        ];
+    };
+  };
+}
